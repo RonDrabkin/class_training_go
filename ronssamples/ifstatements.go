163080f0e0ab6a1d4b53f5c982790518937deac4
RonDrabkin <ron@opened.com>
2016-05-14 21:17:16 -0700

    updating Pong stuff

---
+++ ronssamples/ifstatements.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+
+     
+
+func main() {
+	
+	a:=3
+	b :=true
+
+
+if a !=3 {
+	fmt.Println("hey true")
+} else {
+	fmt.Println("hey false")	
+}
+if food :="chocolate"; b {
+	fmt.Println(food)
+}
+     
+
+}
