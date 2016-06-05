a9f6b60654336c1f29e84bbc743f0b155140dcf2
Ron Drabkin <ron@opened.com>
2016-06-05 13:20:14 -0700

    Update bizzbuzz1000nodoublecount.go

--- ronssamples/first set of exercises/bizzbuzz1000nodoublecount.go
+++ ronssamples/first set of exercises/bizzbuzz1000nodoublecount.go
@@ -1,5 +1,4 @@
-//Here is the solution to https://projecteuler.net/problem=1
-//It was actually pretty close to the fizzbuzz one from the prior set of exercises.  
+ 
 
 
 package main
@@ -20,3 +19,8 @@ func main() {
 	}
 	fmt.Println(total)
 }
+
+//Here is the solution to https://projecteuler.net/problem=1
+//It was actually pretty close to the fizzbuzz one from the prior set of exercises. 
+//It is a for loop up to 1000, where you increment the sum by i if i/3 is a whole number, and also increment by i if i/5 is a whole number.  
+//the tricky part here was to increment by i only once for multiples of 15, I did that wrong at first ...logic issue not programming :)
