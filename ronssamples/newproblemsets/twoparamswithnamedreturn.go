2eaa8404d74cdc7c274fa6114d48b98372ee12b6
RonDrabkin <ron@opened.com>
2016-06-15 16:06:26 -0700

    updating to marshal json

---
+++ ronssamples/newproblemsets/twoparamswithnamedreturn.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+var age int
+func dogyears (age int) (dogyear int) {
+
+return age *7
+}
+
+func main() {
+
+	name := "John"
+	age = 27
+	fmt.Println(name, "is" , dogyears(age))
+	fmt.Println(dogyear)
+
+
+}
+	
+
+
+
+
