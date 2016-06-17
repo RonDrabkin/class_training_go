04914c5d6b8725c34cf6ce9b1f4ce038babfa2b2
RonDrabkin <ron@opened.com>
2016-06-17 16:11:48 -0700

    again

---
+++ ronssamples/pongstuff/addaschoolnapping.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"github.com/openedinc/opened-go"
+	"github.com/jmcvetta/napping"
+)
+
+func main() {
+    
+
+payload := map[string]string{
+  "nces_id":"BB981479",
+  "name":"Woodlands Christian Academy",
+  "address":"5900 Academy Way",
+  "city": "The Woodlands",
+  "state": "TX",
+  "zip": "77384",
+  "low_grade": "K",
+  "high_grade": "12",
+  //it is ok to us a map here not a struct.  examples show a struct but I haven't gotten to that class yet :)
+}
+//jsonParams, _ := json.Marshal(params)
+//client := &http.Client{}
+//req, _ := http.NewRequest("POST", "https://partner.opened.com/1/schools", &jsonParams)
+//req.Header.Add("Content-Type", `W/"application/json"`)
+token,err := opened.GetToken ("","","","") 
+resp, err := napping.Post("https://partner.opened.com/1/schools", &payload, nil, nil)
+if err != nil {
+    fmt.Println("Errored when sending request to the server")
+    return
+}
+//defer resp.Body.Close()
+resp_body , _:= ioutil.ReadAll(napping.body)
+//fmt.Println(resp.Status())
+fmt.Println(string(resp_body))
+
+}
