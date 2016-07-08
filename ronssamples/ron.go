39a343a9badf43cb34476b8cfa2b52b080d7d464
RonDrabkin <ron@opened.com>
2016-07-08 15:51:43 -0700

    is it there

--- ronssamples/ron.go
+++ ronssamples/ron.go
@@ -5,10 +5,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+
 	//"log"
 	"net/http"
-	"net/url"
+	//"net/url"
 )
 
 type Location struct {
@@ -29,32 +29,37 @@ type Location struct {
 
 func main() {
 
-var res Location
+var IPrecord Location
 
 	ip:= "73.223.170.148"
 
-safeURL := url.QueryEscape("https://ipfind.co?ip=ip&auth=b7e99c21-af76-4b21-bf45-463d019c102d") 
+//safeURL := url.QueryEscape("https://ipfind.co?ip=ip&auth=b7e99c21-af76-4b21-bf45-463d019c102d") 
 
-s := fmt.Sprintf(safeURL, "res")
+s := fmt.Sprintf("https://ipfind.co?auth=a3261363-cdc4-47ac-b13e-602445ae7980&ip=%s", ip)
 
-//sprintF with IP
-//http:Get only 
+//s := fmt.Sprintf(safeURL , ip)
+fmt.Println(s)
 
-res, err := http.Get("safeURL")
+
+//in res we load the json payload
+res, err := http.Get(s)
     if err != nil {
-        panic(err.Error())
+       panic(err.Error())
     }
 
-    body, err := ioutil.ReadAll(res.Body)
+ defer res.Body.Close()   
+ fmt.Println(res)
 
-    if err != nil {
-        panic(err.Error())
-    }
-       
- defer res.Body.Close()
+    if err := json.NewDecoder(res.Body).Decode(&record); err != nil {
+ 	log.Println(err)
+ }
 
 
-    json.Unmarshal(body, &res)
+       fmt.Println Iprecord.Latitude in the right fomat
+ /*but 
+
+
+
 
     fmt.Println(s.Latitude, s.Longitude)
    
@@ -69,5 +74,5 @@ res, err := http.Get("safeURL")
  // Use json.Decode for reading streams of JSON data
 //then print those values
 
-
+*/
 }
