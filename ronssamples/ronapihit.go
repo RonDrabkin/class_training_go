c3ad0f18caf7b6dd73462033f34f704f9c0bbbb1
RonDrabkin <ron@opened.com>
2016-07-08 16:53:45 -0700

    api hit works

---
+++ ronssamples/ronapihit.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// A struct is a collection of fields
+// This is our type which matches the JSON object.
+type IpRecord struct {
+	IPAddress     string  `json:"ip_address"`
+	Country       string  `json:"country"`
+	CountryCode   string  `json:"country_code"`
+	Continent     string  `json:"continent"`
+	ContinentCode string  `json:"continent_code"`
+	City          string  `json:"city"`
+	County        string  `json:"county"`
+	Region        string  `json:"region"`
+	RegionCode    string  `json:"region_code"`
+	Timezone      string  `json:"timezone"`
+	Owner         string  `json:"owner"`
+	Longitude     float64 `json:"longitude"`
+	Latitude      float64 `json:"latitude"`
+}
+
+func main() {
+	ip := "122.169.64.203"
+
+	url := fmt.Sprintf("https://ipfind.co?auth=a3261363-cdc4-47ac-b13e-602445ae7980&ip=%s", ip)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal("Get: ", err)
+	}
+
+	defer resp.Body.Close()
+
+	// Fill the record with the data from the JSON
+	var record IpRecord
+
+	// Use json.Decode for reading streams of JSON data
+	if err := json.NewDecoder(resp.Body).Decode(&record); err != nil {
+		log.Println(err)
+	}
+
+	fmt.Printf("Latitude = %f and Longitude = %f", record.Latitude, record.Longitude)
+}
