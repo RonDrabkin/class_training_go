b8532a8b1f436e05213543661be33a0338b0785f
RonDrabkin <ron@opened.com>
2016-08-03 18:24:37 -0700

    fixed, declaring variables correctly

--- ronssamples/methodstocalcshapes.go
+++ ronssamples/methodstocalcshapes.go
@@ -2,29 +2,30 @@ package main
 
 import "fmt"
 
-
-
 type Rectangle struct {
 	side1, side2 int
 }
 
-
-
 func (r Rectangle) perimeter() int {
-	return r.side1 + r.side2
+	return r.side1 + r.side2 + r.side1 + r.side2
 }
 
-//func area
+func (r Rectangle) area() int {
+	return r.side1 * r.side2
+}
 
 func main() {
 
+	var side1 int
+	var side2 int
 
 	fmt.Print("What is side 1? ")
 	fmt.Scan(&side1)
 	fmt.Print("What is side 2? ")
 	fmt.Scan(&side2)
 
-   r := Rectangle{side1, side2}
+	r := Rectangle{side1, side2}
 
-	fmt.Println(r.perimeter())
+	fmt.Println("Periminter is ", r.perimeter())
+	fmt.Println("Area is ", r.area())
 }
